4a74c0b68486e82861b70ba210b36f36ea8440a2
lzheng3 <lekaizheng@icloud.com>
2023-03-18 17:28:38 -0400

    add tailwind css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
